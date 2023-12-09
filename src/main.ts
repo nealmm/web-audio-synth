9067b1309c1ff7a247344b982ce38555e865db10
Matt Neal <neal.matt.m@gmail.com>
2023-12-09 14:26:43 -0500

    Setup astro cactus theme

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
 import './style.css';
+
+const storedTheme = typeof localStorage !== "undefined" && localStorage.getItem("theme");
+const theme = storedTheme || (window.matchMedia("(prefers-color-scheme: light)").matches ? "light" : "dark");
+
+const root = document.documentElement;
+
+if ((theme === "dark" && root.classList.contains("dark")) || (theme === "light" && !root.classList.contains("dark"))) {
+
+}
+else {
+  root.classList.toggle("dark", theme === "dark");
+}
